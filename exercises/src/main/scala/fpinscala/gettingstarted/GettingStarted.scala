1ed371b362eae6201486f8cce434e5014c5df44d
rnjn <runjan@gmail.com>
2015-04-27 11:01:01 +0530

    more of getting started

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -1,7 +1,23 @@
 package in.rnjn.fpinscala.gettingstarted
 
 object First {
+
   def abs(n: Int): Int =
     if (n < 0) -n
     else n
+
+  def factorial(n: Int): Int = {
+    def facti(x: Int, f: Int): Int = {
+      if (x < 1) f
+      else facti(x - 1, x * f)
+    }
+    facti(n, 1)
+  }
+
+  def fibonacci(n: Int): Int = {
+    if (n == 1) 0
+    else if (n == 2) 1
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
 }
--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
@@ -15,4 +15,45 @@ class GettingStartedSpec extends FunSuite {
     assert(First.abs(0) == 0)
   }
 
+  test("0! is 1") {
+    assert(First.factorial(0) == 1)
+  }
+
+  test("1! is 1") {
+    assert(First.factorial(1) == 1)
+  }
+
+  test("5! is 120") {
+    assert(First.factorial(5) == 120)
+  }
+
+  test("1st fibonacci number is 1") {
+    assert(First.fibonacci(1) == 0)
+  }
+
+  test("2nd fibonacci number is 1") {
+    assert(First.fibonacci(2) == 1)
+  }
+
+  test("3rd fibonacci number is 1") {
+    assert(First.fibonacci(3) == 1)
+  }
+
+  test("4th fibonacci number is 2") {
+    assert(First.fibonacci(4) == 2)
+  }
+
+  test("5th fibonacci number is 3") {
+    assert(First.fibonacci(5) == 3)
+  }
+
+  test("10th fibonacci number is 34") {
+    assert(First.fibonacci(10) == 34)
+  }
+
+
+  test("20th fibonacci number is 4181") {
+    assert(First.fibonacci(20) == 4181)
+  }
 }
+
