2a2d938e517fd20b62ac749c8ee16cd2ac6b2a32
rnjn <runjan@gmail.com>
2015-07-21 15:27:37 +0530

    take and take while

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -2,14 +2,37 @@ package in.rnjn.fpinscala.laziness
 
 trait Stream[+A] {
 
+  def head: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def tail: Option[Stream[A]] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(t())
+  }
+
   def toList: List[A] = this match {
     case Empty => List()
     case Cons(h, t) => h() :: t().toList
   }
 
-  def take(n: Integer): Stream[A] = {
-    Empty
-  }
+  def take(n: Integer): Stream[A] =
+      this match {
+        case Empty => Empty
+        case Cons(h, t) =>
+          if(n > 0)
+            Cons(h, () => t().take(n - 1))
+          else Empty
+      }
+
+  def takeWhile(p: A => Boolean): Stream[A] =
+    this match {
+      case Empty => Empty
+      case Cons(h, t) =>
+        if(p(h())) Cons(h, () => t().takeWhile(p))
+        else Empty
+    }
 }
 
 case object Empty extends Stream[Nothing]
--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -16,9 +16,18 @@ class StreamSpec extends FunSuite {
   }
 
   val oneToFourStream = Cons[Integer](() => 1, oneToFour(1))
+
   test("natural numbers 1-4 to list") {
     assert(List(1, 2, 3, 4) == oneToFourStream.toList)
   }
 
+  test("take 2 from Stream 1,2,3,4 to get Stream 1,2"){
+    val substream = oneToFourStream.take(2)
+    assert(List(1, 2) ==  substream.toList)
+  }
+  test("take while n < 4 from Stream 1,2,3,4 to get Stream 1,2,3"){
+    val substream = oneToFourStream.takeWhile((i: Integer) => i < 4)
+    assert(List(1, 2, 3) ==  substream.toList)
+  }
 
 }
